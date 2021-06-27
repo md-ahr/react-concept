0d302749cb509937b90c2c0453be415fb649d4cf
Abdul Halim <halim@okomari.com>
2021-06-27 18:02:07 +0600

    State change on clock component

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import { Component } from 'react';
 
-class Clock extends React.Component {
+class Clock extends Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         date: new Date()
+    //     };
+    // }
+
+    state = {
+        date: new Date()
+    };
+
+    componentDidMount() {
+        this.clockTimer = setInterval(() => { this.tick(); }, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.clockTimer);
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        console.log('Button was clicked!');
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
     render() {
+        const { date } = this.state;
         return (
-            <h1 className="heading">
-                <span className="text">{new Date().toLocaleTimeString(this.props.local)}</span>
-            </h1>
+            <div>
+                <h1 className="heading">
+                    <span>Clock: </span>
+                    <span className="text">{date.toLocaleTimeString(this.props.local)}</span>
+                </h1>
+                <a href="/" onClick={this.handleClick}>Click Me</a>
+            </div>
         );
     }
+
 }
 
 export default Clock;
